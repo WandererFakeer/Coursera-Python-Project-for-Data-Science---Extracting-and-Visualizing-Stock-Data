22feb23a91412599b735ec7acab6ce5db723ed9a
Sayani Mondal (Wanderer) <sayanimondal604@gmail.com>
2025-01-31 20:45:08 +0530

    06. Plot Tesla Stock Graph.py

---
+++ 06. Plot Tesla Stock Graph.py
@@ -0,0 +1,5 @@
+# Use the make_graph function, with tesla_data, tesla_revenue , string - "Tesla's Historical Share Price and Historical Revenue (Till June 2021)" as parameters, 
+ # to graph the Tesla Stock Data. 
+ # Provide a title for the graph. (Name - Tesla's Historical Share Price and Historical Revenue (Till June 2021))
+
+make_graph(tesla_data, tesla_revenue, "Tesla's Historical Share Price and Historical Revenue (Till June 2021)")
