544ee85d15804ebc5de6006a9b41442d5a554d21
Sayani Mondal (Wanderer) <sayanimondal604@gmail.com>
2025-01-31 20:46:01 +0530

    07. Plot GameStop Stock Graph.py

---
+++ 07. Plot GameStop Stock Graph.py
@@ -0,0 +1,5 @@
+# Use the make_graph function, with gme_data, gme_revenue , string - "GameStop's Historical Share Price and Historical Revenue (Till June 2021)" as parameters, 
+ # to graph the Tesla Stock Data. 
+ # Provide a title for the graph. (Name - GameStop's Historical Share Price and Historical Revenue (Till June 2021))
+
+make_graph(gme_data, gme_revenue, "GameStop's Historical Share Price and Historical Revenue (Till June 2021)")
